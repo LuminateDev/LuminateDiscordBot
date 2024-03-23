8f79aa95b13ce559bcc046e171ea48068b19f570
Fabio Gaming <fabio.furch@gmail.com>
2024-03-23 01:30:00 +0100

    added bot base

---
+++ LuminateDiscordBot/DBManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuminateDiscordBot
+{
+    internal class DBManager
+    {
+        static string connectionString = "Data Source=LuminateConfig/database.db;Version=3;";
+
+        public static void InitDB()
+        {
+            SQLiteConnection.CreateFile(connectionString);
+            ExecuteNonQuery("CREATE TABLE IF NOT EXISTS WelcomeConfig(WelcomeChannelId BIGINT DEFAULT 0, WelcomeMessage TEXT)");
+            ExecuteNonQuery("CREATE TABLE IF NOT EXISTS TicketConfig(TicketCategoryId BIGING DEFAULT 0)");
+            ExecuteNonQuery("CREATE TABLE IF NOT EXISTS UserStats(DiscordId BIGINT, MessagesSent BIGINT DEFAULT 0, Level INT DEFAULT 1, XP INT DEFAULT 0)");
+            ExecuteNonQuery("CREATE TABLE IF NOT EXISTS SelectionRoles(RoleId BIGINT)");
+
+
+        }
+
+
+
+        static void ExecuteNonQuery(string query)
+        {
+            using (SQLiteConnection connection = new(connectionString))
+            {
+                connection.Open();
+
+                using(SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = query;
+                    command.ExecuteNonQuery();
+                }
+                    
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ LuminateDiscordBot/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LuminateDiscordBot
+{
+    internal class Utils
+    {
+
+
+        public static void FileCheck()
+        {
+            if(!Directory.Exists("LuminateConfig")) { CreateFiles(); }
+            if(!File.Exists("LuminateConfig/config.json")) { CreateFiles(); }
+        }
+
+
+        static void CreateFiles()
+        {
+            Directory.CreateDirectory("LuminateConfig");
+            using (StreamWriter sw = File.CreateText("LuminateConfig/config.json")) { sw.Write(JsonSerializer.Serialize(new Objects.Config(), new JsonSerializerOptions { WriteIndented = true })); }
+        }
+    }
+}

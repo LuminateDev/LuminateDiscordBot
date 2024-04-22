c6a2a25a7e773e5b2fc425ec383d77137ca4d368
Fabio Gaming <fabio.furch@gmail.com>
2024-04-22 11:23:22 +0200

    Added more base code
    [ - ] Removed unused using statements
    [ # ] HandleTicketMenu is almost ready
    [ + ] Added HandleTicketDismiss to handle auto response deletions
    [ + ] Added HandleTicketForce base function to handle forced tickets

--- LuminateDiscordBot/Components.cs
+++ LuminateDiscordBot/Components.cs
@@ -1,11 +1,6 @@
 ﻿using Discord;
 using Discord.Interactions;
-using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LuminateDiscordBot.Objects;
 
 namespace LuminateDiscordBot
 {
@@ -15,7 +10,41 @@ namespace LuminateDiscordBot
         [ComponentInteraction("ticket-start")]
         public async Task HandleTicketMenu()
         {
-            
+            IComponentInteraction interaction = (IComponentInteraction)Context.Interaction;
+
+            TicketCategory ticket = DBManager.GetTicketCategoryFromName(interaction.Data.Values.First());
+            if(ticket.TicketDataAutoResponse != null)
+            {
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.Title = "Attention.";
+                embed.Description = ticket.TicketDataAutoResponse;
+                embed.Color = Color.Blue;
+                embed.Footer = new EmbedFooterBuilder()
+                {
+                    Text = "Luminate - Your ideas shine bright"
+                };
+
+                ComponentBuilder components = new ComponentBuilder();
+                components.WithButton("Open a ticket anyways", "ticekt-force", ButtonStyle.Success);
+                components.WithButton("Dismiss", "ticket-dismiss", ButtonStyle.Danger);
+
+                await RespondAsync("", new[] { embed.Build() }, ephemeral:true);
+                return;
+            }
+
+            // ADD CHANNEL CREATE LOGIC
+        }
+
+        [ComponentInteraction("ticket-dismiss")]
+        public async Task HandleTicketDismiss()
+        {
+            await DeleteOriginalResponseAsync();
+        }
+
+        [ComponentInteraction("ticket-force")]
+        public async Task HandleTicketForce()
+        {
+            // HANDLE CHANNEL CREATE LOGIC
         }
     }
 }

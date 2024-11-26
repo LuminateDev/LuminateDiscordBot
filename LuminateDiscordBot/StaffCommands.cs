84645897de40a91e3b8797867262a3b846d6530f
FabioGaming <fabio.furch@gmail.com>
2024-11-26 09:06:13 +0100

    started moving commands into seperate classes
    [ # ] Started to move Commands into its own classes like UserCommands and StaffCommands to seperate them and make them eaasier to read and modify later on

---
+++ LuminateDiscordBot/StaffCommands.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuminateDiscordBot
+{
+    [Group("staff", "Contains all staff commands")]
+    [RequireUserPermission(Discord.GuildPermission.Administrator)]
+    [CommandContextType(Discord.InteractionContextType.Guild)]
+    public class StaffCommands :InteractionModuleBase
+    {
+        private readonly DataContext _dataContext;
+        public StaffCommands(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+    }
+}
---
+++ LuminateDiscordBot/UserCommands.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuminateDiscordBot
+{
+    [CommandContextType(Discord.InteractionContextType.Guild)]
+    public class UserCommands : InteractionModuleBase
+    {
+        private readonly DataContext _dataContext;
+        public UserCommands(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+    }
+}
